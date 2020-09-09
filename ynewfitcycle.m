c783265cba3db98849d06b48168fe59304ea469e
niujin2013 <31968235+niujin2013@users.noreply.github.com>
2020-09-09 13:17:35 -0400

    Function for integration with cell cycle

--- ynewfitcycle.m
+++ ynewfitcycle.m
@@ -4,14 +4,14 @@
 % Returns expected infection values given a set of time points and 
 %   coefficients 
 % Inputs: 
-% a – List of free parameter values 
-% x – List of time points of interest 
+% a â€“ List of free parameter values 
+% x â€“ List of time points of interest 
 % Output: 
-% y – A list of expected value for each time 
-%     point in ‘x’. 
+% y â€“ A list of expected value for each time 
+%     point in â€˜xâ€™. 
 function y = ynewfitcycle(param, time  ) 
   % Define beta and delta as globals so we can pass the values into the 
-  % ‘IDR1’ function.
+  % â€˜IDR1â€™ function.
  global beta_prev IC_1 IC_2 IC_3 IC_4 IC_5 IC_6 ; 
  beta_prev = xlsread('C:\Users\jinniu\Documents\MATLAB\new panc1 proteins\cIAP\parameter seq.xls', 'B2:B20');
  global ELYS_MA ASPP2_AP  kapm0 cIAP_AP ktau; 
@@ -34,32 +34,7 @@ function y = ynewfitcycle(param, time  )
   % Solve our model.  
   [t, y] = ode15s(@IDRnewcycle, [0 time(end)], ICS) ; %ODE(dF, [T], [ICS])
    
-%         live_B = y(:,41) +y(:,42) +y(:,43)+ y(:,44)+ y(:,45) ; %  !total live cells :B
-%         live_P = y(:,47) +y(:,48) +y(:,49)+ y(:,50)+ y(:,51) ; %  !total live cells :P
-%         live_BP= y(:,53) +y(:,54) +y(:,55)+ y(:,56)+ y(:,57) ; %  !total live cells :C
-%         
-%   cycle_B = y(:, 41:45) ./ live_B .*100 ; 
-%   cycle_P = y(:, 47:51) ./ live_P .*100 ;  
-%   cycle_BP= y(:, 53:57) ./ live_BP.*100 ;    
-%   
-%   cycle_B1 = [cycle_B(:, 1:2) cycle_B(:, 3)+ cycle_B(:,4) cycle_B(:, 5)] ; 
-%   cycle_P1 = [cycle_P(:, 1:2) cycle_P(:, 3)+ cycle_P(:,4) cycle_P(:, 5)] ;   
-%   cycle_BP1= [cycle_BP(:, 1:2) cycle_BP(:, 3)+ cycle_BP(:,4) cycle_BP(:, 5)] ;   
-%   
-%   apo_B =  y(:, 46) ./ (live_B+ y(:, 46)) *100 ; 
-%   apo_P =  y(:, 52) ./ (live_P+ y(:, 52)) *100 ;   
-%   apo_BP=  y(:, 58) ./ (live_BP+y(:, 58)) *100 ; 
-%   
-%   N_B  =(live_B+ y(:, 46)) ; 
-%   N_P  =(live_P+ y(:, 52)) ;
-%   N_BP =(live_BP+y(:, 58)) ;
-
-%OUT   
-%   OUT = [cycle_B1 apo_B N_B cycle_P1 apo_P N_P cycle_BP1 apo_BP N_BP ]; %PERCENTAGES      
-%   ALL = interp1(t, y , time ) ;  
-%   y =  interp1(t, OUT, time ) ;
-%  
-%   y =  reshape(y, size(y,1 )*size(y,2 ) ,1)  ; 
+ 
 
 forB = [y(:, 41)  y(:, 42) y(:, 43)+y(:, 44) y(:, 45) y(:, 46) ]; 
 forP = [y(:, 47)  y(:, 48) y(:, 49)+y(:, 50) y(:, 51) y(:, 52) ]; 
